cb9f8277baca2890c886f9e287b5304fe29be95b
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2023-06-05 15:37:21 +0700

    feat: validate overlap sleep time

--- app/models/sleep.rb
+++ app/models/sleep.rb
@@ -4,13 +4,31 @@ class Sleep < ApplicationRecord
     before_save :calculate_duration
 
     validates_comparison_of :wakeup_at, greater_than: :sleep_at
+    validate :no_overlap
+
+    scope :time_overlap, -> (sleep_at, wakeup_at) {
+        wakeup_at = Time.parse(wakeup_at) if wakeup_at.is_a? String
+        sleep_at = Time.parse(sleep_at) if sleep_at.is_a? String
+
+        where('sleep_at < ?', wakeup_at).
+        where('? < wakeup_at', sleep_at)
+    }
 
     def as_json(options = nil)
         super({ only: [:sleep_at, :wakeup_at, :duration] }.merge(options || {}))
     end
 
+
     private
         def calculate_duration
             self.duration = wakeup_at - sleep_at
+        end
+
+        def no_overlap
+            errors.add(:sleep_at, "sleep_at overlap") if
+            Sleep.time_overlap(self.sleep_at, self.wakeup_at).
+                where(user_id: self.user_id).
+                where.not(id: self.id).
+                exists?
         end
 end
--- test/models/sleep_test.rb
+++ test/models/sleep_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class SleepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validate wakeup_at greater sleep_at" do
+    user = users(:john)
+
+    s1 = Sleep.new(
+      user_id: user.id,
+      wakeup_at: "2021-02-10T04:00:00",
+      sleep_at: "2021-02-10T05:00:00"
+    )
+    assert !s1.valid?
+
+    s2 = Sleep.new(
+      user_id: user.id,
+      sleep_at: s1.wakeup_at,
+      wakeup_at: s1.sleep_at,
+    )
+
+    assert s2.valid?
+  end
+
+  test "validate no overlap" do
+    bob = users(:bob)
+    john = users(:john)
+
+    bob.sleeps.create!(sleep_at: "2021-02-10T04:00:00", wakeup_at: "2021-02-10T10:00:00")
+
+    s1 = john.sleeps.new(sleep_at: "2021-02-10T08:00:00", wakeup_at: "2021-02-10T12:00:00")
+    assert s1.valid?
+
+    s2 = bob.sleeps.new(sleep_at: "2021-02-10T08:00:00", wakeup_at: "2021-02-10T12:00:00")
+
+    assert !s2.valid?
+    assert_equal 'sleep_at overlap', s2.errors.messages[:sleep_at].first
+  end
 end
